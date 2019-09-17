e4bb5953e199d6d70fb2c534796bec5bf4602308
github username <github email address>
2019-09-17 14:11:38 +0100

    Got it workinggit commit -mgit commit -m

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -17,34 +17,22 @@ def get_character_movies_from_api(character_name)
         # binding.pry
         # puts "Done"
       end 
-
     end 
-
   end 
   film_array
 end 
 
-  #   films_array = get_character_movies_from_api(character)
-  #   print_movies(films_array
-  # end 
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
-
-
 def print_films(films)
-  puts get_character_movies_from_api(films)
+films.each do |film|
+  puts film['title']
 end
+end 
 
 def show_character_movies(character)
   films = get_character_movies_from_api(character)
   print_films(films)
 end
+
 
 ## BONUS
 
