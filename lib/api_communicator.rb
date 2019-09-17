67aee2761ba03ab80ffe6acec5fd2c7609b49a72
Tim Golding <tgolding55@hotmail.com>
2019-09-17 14:41:03 +0100

    Outputs films, if you enter the character correctly

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -8,8 +8,10 @@ def get_character_movies_from_api(character_name)
   response_hash = JSON.parse(response_string)
 
   character_list = get_results(response_hash)
-  found_character = find_characeter(character_list, character_name)
-
+  found_character = find_character(character_list, character_name)
+  found_films = find_films(found_character)
+  got_films = get_films(found_films)
+return got_films
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,8 +21,7 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
-binding.pry
-0
+
 end
 
 def get_results(response_hash)
@@ -29,14 +30,45 @@ end
 
 def find_character(character_list, character_name)
   character_list.each do |character_loop|
-    get_character_name(character_loop)
+    character = get_character_name(character_loop)
+    if character == character_name
+      return character_loop
+    end
   end
 end
 
+def find_films(found_character)
+films = found_character["films"]
+end
 
+def get_character_name(character_loop)
+  character_loop["name"]
+end
+
+def get_films(found_films)
+  api_films = []
+  found_films.each do |api|
+    api_films.push(JSON.parse(RestClient.get(api)))
+  end
+  api_films
+end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  print_films = title_finder(films)
+  print_films.each do |film|
+    puts(film)
+  end
+end
+
+def title_finder(films)
+  titles = []
+
+  films.each do |film|
+    titles.push(film["title"])
+  end
+
+  return titles
 end
 
 def show_character_movies(character)
